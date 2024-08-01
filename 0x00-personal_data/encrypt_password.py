2d631c10519cd904b1d4c425a856bc89c76043aa
shumani <shumanisimeli@gmail.com>
2024-08-01 23:22:38 +0200

    first commit

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""encrypt_password.py"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """returns a salted, hashed password"""
+    password_byte = str.encode(password)
+    return bcrypt.hashpw(password_byte, bcrypt.gensalt())
+
+
+def is_valid(hashed_password, password):
+    """validate that the provided password matches the hashed password"""
+    if bcrypt.checkpw(str.encode(password), hashed_password):
+        return True
+    return False
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""filtered_logger.py"""
+import logging
+import re
+import os
+import mysql.connector
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "ssn", "password", "date_of_birth")
+
+
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
+    for field in fields:
+        pattern = f'{field}=[^{separator}]*'
+        message = re.sub(pattern, f"{field}={redaction}", message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """returns formatted string"""
+        message = super().format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+
+
+def main():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    logger = get_logger()
+
+    for row in cursor:
+        row_dict = {
+            "name": row[0],
+            "email": row[1],
+            "phone": row[2],
+            "ssn": row[3],
+            "password": row[4],
+            "ip": row[5],
+            "last_login": row[6],
+            "user_agent": row[7]
+        }
+        message = "; ".join([f"{key}={value}" for key, value in row_dict.items()]) + ";"
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
