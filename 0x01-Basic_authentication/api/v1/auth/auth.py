daf09b397f25bf7a371e566258f4dbc314b8a630
shumani <shumanisimeli@gmail.com>
2024-08-07 16:26:50 +0200

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,7 @@ class Auth():
             return None
         if 'Authorization' not in request.headers:
             return None
-        return request.headers['Authorization']
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Handles current user
