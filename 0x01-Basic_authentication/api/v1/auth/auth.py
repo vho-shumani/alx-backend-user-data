e9d4431933985a421187898b8d31680fb9bb9526
shumani <shumanisimeli@gmail.com>
2024-08-07 15:46:12 +0200

    updated auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,7 @@ from typing import List, TypeVar
 class Auth():
     """Manages the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"""
+        """Checks if authenication is required"""
         if path and path[-1] != '/':
             path += '/'
         if not path or not excluded_paths:
