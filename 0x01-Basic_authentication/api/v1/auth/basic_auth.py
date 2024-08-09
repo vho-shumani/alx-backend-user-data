bdd85c4cc37ad890437180eeac2f640ff6e071bc
shumani <shumanisimeli@gmail.com>
2024-08-10 00:03:56 +0200

    updated basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,7 +47,8 @@ class BasicAuth(Auth):
             return (None, None)
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
-        email, password = decoded_base64_authorization_header.split(':')
+        email = decoded_base64_authorization_header.split(':')[0]
+        password = ":".join(decoded_base64_authorization_header.split(':')[1:])
         return (email, password)
 
     def user_object_from_credentials(self,
