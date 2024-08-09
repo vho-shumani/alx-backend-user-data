578397e01aeef8452863f6e5de8825fcf88c1a26
shumani <shumanisimeli@gmail.com>
2024-08-09 20:58:46 +0200

    fixed pycodestyle error on basic_auth file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -82,7 +82,7 @@ class BasicAuth(Auth):
         auth_header = self.authorization_header(request)
         if not auth_header:
             return None
-        
+
         base64_auth = self.extract_base64_authorization_header(auth_header)
         if not base64_auth:
             return None
@@ -90,7 +90,7 @@ class BasicAuth(Auth):
         decoded_auth = self.decode_base64_authorization_header(base64_auth)
         if not decoded_auth:
             return None
-        
+
         user_email, password = self.extract_user_credentials(decoded_auth)
         if not user_email or not password:
             return None
