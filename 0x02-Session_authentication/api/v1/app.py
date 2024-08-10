6daba759c54417de1133e1be7f5fbb99e129f571
shumani <shumanisimeli@gmail.com>
2024-08-10 20:19:15 +0200

    uopdate

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,17 +50,23 @@ def not_found(error) -> str:
 def before_request():
     """run before each request
     """
-    request.current_user = auth.current_user(request)
+    
     if auth:
         excluded_paths = ['/api/v1/status/',
                           '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']
+                          '/api/v1/forbidden/', 
+                          '/api/v1/auth_session/login/'
+                          ]
         if not auth.require_auth(request.path, excluded_paths):
             return
-        if auth.authorization_header(request) is None:
+        
+        if not auth.authorization_header(request) and not auth.session_cookie(request):
             abort(401)
+
+        request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
             abort(403)
+
 
 
 if __name__ == "__main__":
