c42004c4815ca95dc8407a587d7c1f7d329880f5
shumani <shumanisimeli@gmail.com>
2024-08-14 22:50:23 +0200

    update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import logging
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -46,14 +47,14 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email: str) -> str | None:
+    def create_session(self, email: str) -> Union[str, None]:
         """Create a session id for a user"""
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
-        except Exception:
+        except NoResultFound:
             return None
 
     def get_user_from_session_id(self, session_id: str) -> User:
