8729203481cdbf62a69132adf84f99155d9d304b
shumani <shumanisimeli@gmail.com>
2024-08-14 15:04:24 +0200

    updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,16 +8,17 @@ from sqlalchemy.orm.exc import NoResultFound
 import uuid
 
 
+def _hash_password(password: str) -> bytes:
+    """hashes password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """hashes password"""
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     def register_user(self, email: str, password: str) -> User:
         """register a user"""
@@ -27,7 +28,7 @@ class Auth:
         except NoResultFound:
             pass
         hashed_pass = self._hash_password(password)
-        return self._db.add_user(email, hashed_pass) 
+        return self._db.add_user(email, hashed_pass)
 
     def valid_login(self, email: str, password: str) -> bool:
         """validates the login credentials"""
@@ -38,13 +39,13 @@ class Auth:
             else:
                 return False
         except NoResultFound:
-            return False           
+            return False
 
     def _generate_uuid(self) -> str:
         """generates a uuid"""
         return str(uuid.uuid4())
-    
-    def create_session(self, email:str) -> str:
+
+    def create_session(self, email: str) -> str:
         """Create a session id for a user"""
         try:
             user = self._db.find_user_by(email=email)
@@ -78,13 +79,15 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError("user does not exist")
-        
+
     def update_password(self, reset_token: str, password: str) -> None:
         """updates a user password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = self._hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
+            self._db.update_user(user.id,
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
             return None
         except NoResultFound:
             raise ValueError("user doesnot exist.")
