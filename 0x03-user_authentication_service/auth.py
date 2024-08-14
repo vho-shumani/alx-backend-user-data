f701b42dbddd8e2102e5155ee0c24592aa9c3dac
shumani <shumanisimeli@gmail.com>
2024-08-14 19:53:25 +0200

    update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,7 +54,7 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
             return session_id
         except NoResultFound:
-            pass
+            return None
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """Retrieves user using session_id"""
